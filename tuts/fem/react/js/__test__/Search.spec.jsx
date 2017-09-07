83d6033e076a9be7de633148af3c8c4b2d65ac1e
markfranciose <mark.franciose@gmail.com>
2017-09-07 16:18:36 -0500

    add test for correct amount of shows

--- tuts/fem/react/js/__test__/Search.spec.jsx
+++ tuts/fem/react/js/__test__/Search.spec.jsx
@@ -1,9 +1,23 @@
 import React from "react";
-import renderer from "react-test-renderer";
+// import renderer from "react-test-renderer";
+import preload from "../../data.json";
+import { shallow } from "enzyme";
 import Search from "../Search";
+import ShowCard from "../ShowCard";
 
-test("Search render correctly", () => {
-	const component = renderer.create(<Search />);
-	const tree = component.toJSON();
-	expect(tree).toMatchSnapshot();
+test("Search renders correctly", () => {
+	const component = shallow(<Search />);
+	expect(component).toMatchSnapshot();
+});
+
+test("Search should render the correct amount of shows", () => {
+	const component = shallow(<Search />);
+	expect(component.find(ShowCard).length).toEqual(preload.shows.length);
+});
+
+test("Search should render correct amount of shows based on search term", () => {
+	const searchWord = "black";
+	const component = shallow(<Search />);
+	component.find("input").simulate("change", { target: { value: searchWord } });
+	// const showCount = ~3:30, setting this ugly thing up
 });
