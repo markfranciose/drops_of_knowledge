4049fa468635eaac09e5c0e9f21f5f8ced9c1a6b
markfranciose <mark.franciose@gmail.com>
2017-09-09 21:58:19 -0500

    map through json to display rests

--- sandboxes/react-sandbox/src/App.js
+++ sandboxes/react-sandbox/src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import RestCard from './RestCard'
+import RestCard from './RestCard';
+import preload from './restaurants.json';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <RestCard name='Gene and George'/>
-      <RestCard name='Riva'/>
-      <RestCard name='Mercat'/>
+      <div>
+        {preload.restaurants.map(rest => <RestCard name={rest.name} />)}
+      </div>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Eat Union while you're in Chicago!</h2>
