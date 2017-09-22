82b2e921b877fa39605af563835451b17028be5c
Mark Franciose <mark.franciose@gmail.com>
2017-09-22 17:06:36 -0500

    first lesson of codecademy java

---
+++ practice/java-cc/java-start.java
@@ -0,0 +1,94 @@
+public class DataTypesBoolean {
+	public static void main(String[] args) {
+		
+		System.out.println(true);
+		System.out.println(false); 
+
+	}
+}
+
+public class DataTypesChar {
+	public static void main(String[] args) {
+		
+		System.out.println('G');
+
+	}
+}
+
+// Char values are enclosed in single quotes
+
+public class DataTypesString {
+	public static void main(String[] args) {
+	
+		System.out.println("Strings are enclosed in double quotes.")
+
+	}
+}
+
+public class Variables {
+	public static void main(String[] args) {
+	
+		int myNumber = 100;
+		boolean isFun = true;
+		char movieRating = 'A';
+
+	}
+}
+
+public class WhiteSpace {
+	public static void main(String[] args) {
+	
+		boolean isFormatted = false;
+		System.out.println(isFormatted);
+	
+	}
+}
+
+// Java uses the standard operators
+
+public class StandardOperators {
+	public static void main(String[] args) {
+	
+		int addedNum = 2 + 3;
+		int subtractedNum = 3 - 2;
+		int multipliedNum = 3 * 2;
+		int dividedNum = 3 / 2;
+	
+	}
+}
+
+public class Modulo {
+	public static void main(String[] args) {
+	
+		int myRemainder = 5 % 3;	
+	
+	}
+}
+
+// Standard relational operators as well.
+
+public class RelationalOperators { 
+	public static void main(String[] args) {
+
+		System.out.println(3 <= 100); // will print out 'true', as it evaluates the expression
+
+	}
+}
+
+
+public class EqualityOperators {
+	public static void main (String[] args) {
+	
+		System.out.print(3 != 4);
+		System.out.print(100 == (101 -1));
+	
+	}
+}
+
+
+
+
+
+
+
+
