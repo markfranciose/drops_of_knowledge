bad12e90a8b98b98ef63c55c34b2d0bafc13acce
Mark Franciose <mark.franciose@gmail.com>
2017-09-26 09:22:16 -0500

    able to print array to console

---
+++ practice/java-cc/quicksort.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.Arrays;
+
+public class QuickSort {
+    public static int[] sorter(int[] unsortedArray) {
+        return unsortedArray;
+    }
+
+    public static void main(String[] args) {
+        int[] cool = {1,2,3,4};
+        System.out.println(Arrays.toString(sorter(cool)));
+    }
+}
