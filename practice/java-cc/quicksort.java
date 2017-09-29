743b50999299a35d09a83d3b8044ff503631b7c2
Mark Franciose <mark.franciose@gmail.com>
2017-09-29 13:51:45 -0500

    add new ruby tricks, work on java quicksort

--- practice/java-cc/quicksort.java
+++ practice/java-cc/quicksort.java
@@ -1,13 +1,22 @@
 package com.company;
 import java.util.Arrays;
+import java.util.*;
 
 public class QuickSort {
-    public static int[] sorter(int[] unsortedArray) {
-        return unsortedArray;
+    public static int sorter(int[] unsortedArray) {
+				int pivot = unsortedArray[unsortedArray.length - 1];
+				int[] left = [];
+				int[] right = [];
+				for (int i = 0; i < unsortedArray.length; i += 1) {
+					if (unsortedArray[i] <= pivot) {
+
+					}	
+				}
+        return pivot;
     }
 
     public static void main(String[] args) {
         int[] cool = {1,2,3,4};
-        System.out.println(Arrays.toString(sorter(cool)));
+        System.out.println(sorter(cool));
     }
 }
