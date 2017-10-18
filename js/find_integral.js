50de61c0cadac8fa625579318640b6fd097f4ec6
markfranciose <mark.franciose@gmail.com>
2017-10-17 21:26:51 -0500

    add integral kata

---
+++ js/find_integral.js
@@ -0,0 +1,25 @@
+function integrate(co, exponent) {
+	let exponentString = (exponent + 1).toString();
+	let coString = (co / (exponent + 1)).toString();
+
+	return coString + "x^" + exponentString;
+}
+
+let b = integrate(3,2);
+let c = integrate(9,5);
+
+console.log(b);
+console.log(c);
+
+// the function below works because when adding to a string,
+// JS coerses the number into a string.
+
+integrate = (co, ex) => co / (ex + 1) + "x^" + (ex + 1);
+
+
+let q = integrate(3,2);
+let a = integrate(9,5);
+
+console.log(q);
+console.log(a);
+
