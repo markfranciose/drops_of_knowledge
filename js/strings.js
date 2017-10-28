dc06f51938f7dad79a9159add0915e7c06cb5c33
Mark Franciose <mark.franciose@gmail.com>
2017-10-28 17:27:00 -0500

    add JS String.match() and String.include()

--- js/strings.js
+++ js/strings.js
@@ -18,7 +18,6 @@ console.log(notAnymore);
 
 // Creating a hashmap of words and # of occurances in a string
 
-```
 var string = "How much wood could a wood chuck chuck if a wood chuck could";
 
 var counts = {};
@@ -31,7 +30,6 @@ for (var i = 0; i < string.length; i += 1) {
 
 	counts[char] = count ? count + 1 : 1;
 }
-```
 
 // Count the number of ocurrances of a substring within a string.
 
@@ -39,3 +37,35 @@ var temp = "This is a string is";
 var count = (temp.match(/is/g) || []).length;
 console.log(count); // returns 3, 'th*is is is'
 
+// String.includes() function:
+// works as expected
+
+console.log("The dog ordered Dominos".includes('dog')); // logs true
+
+// String.match() function
+// match has some quirks to it...
+
+// basic syntax is:
+let matchString = "I am going to match this string";
+
+let matchRegexp = /match/
+let noMatchRegexp = /Tom Hardy/
+
+let firstMatch = matchString.match(matchRegexp);
+let secondMatch = matchString.match(noMatchRegexp)
+
+console.log(firstMatch); // returns an object. 
+
+console.log(secondMatch); // logs null
+
+// useful thing you can do with String.match(): count matches within a string.
+// eg. how many times does 'ok' exist within a given string.
+//
+
+let okString = "okokokokokokokkkokokokokkokokokkoookkkookokokokokokoookkkkookokokokoo";
+let okRegexp = /ok/g
+// using the global flag on the regex will return an array of only the matching instances. 
+
+let okMatchCount = okString.match(okRegexp).length
+
+console.log(okMatchCount);
