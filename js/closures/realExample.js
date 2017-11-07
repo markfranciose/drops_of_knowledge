9f977dc780cdb034cf49b9786c5df51ed3ee95f8
markfranciose <mark.franciose@gmail.com>
2017-11-07 13:40:02 -0600

    update color page on closures, for Victor

--- js/closures/realExample.js
+++ js/closures/realExample.js
@@ -9,10 +9,14 @@ document.addEventListener('DOMContentLoaded', function(){
 	var changeGreen = colorChanger("green");
 	var changeRed = colorChanger("red");
 	var changeBlue = colorChanger("blue");
+	let changeYellow = colorChanger("yellow");
+	let changePurple = colorChanger("purple");
 
 	document.getElementById('green').onclick = changeGreen;
 	document.getElementById('red').onclick = changeRed;
 	document.getElementById('blue').onclick = changeBlue;
+	document.getElementById('yellow').onclick = changeYellow;
+	document.getElementById('purple').onclick = changePurple;
 
 	document.getElementById('orange').onclick = function() { document.body.style.backgroundColor = "orange"; }
 	
