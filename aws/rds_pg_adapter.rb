7a9b4f6be8daf83b37c118db0d60805301ab8ac9
Mark Franciose <mark.franciose@gmail.com>
2017-11-16 21:27:38 -0600

    adapter not connecting

--- aws/rds_pg_adapter.rb
+++ aws/rds_pg_adapter.rb
@@ -4,5 +4,6 @@ PG::Connection.new(
 	host: "mark-productivity-devity.cepzydtxzbqf.us-east-2.rds.amazonaws.com",
 	port: 5432,
 	user: "markfranciose",
+	password: "asdf",
 	dbname: "mark_productivity",
 	connect_timeout: 4000)
