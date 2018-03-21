ae6ab70d7ec11101786f71070acb30866188e3de
Mark Franciose <mark.franciose@gmail.com>
2018-03-20 23:51:44 -0500

    add files to python dir

---
+++ python/dict.py
@@ -0,0 +1,15 @@
+d = {
+    1: "uno",
+    2: "dos",
+    3: "tres",
+    4: "four"
+}
+
+# keys
+print(d.keys())
+
+# values
+print(d.values())
+
+# both - list of tuples
+print(d.items())
---
+++ python/string.py
@@ -0,0 +1,15 @@
+# Join method
+# join string . join (sequence to be joined )
+string = " "
+sequence = ("a", "b", "c", "d")
+print(string.join(sequence))
+
+#multi line string multiline
+multiline_string = """
+THIS IS 
+A
+MULTILINE
+STRING!
+"""
+
+print(multiline_string)
