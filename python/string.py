330ca3c36baf21afde45dee740498f85ef197ea4
Mark Franciose <mark.franciose@gmail.com>
2018-03-21 23:24:00 -0500

    add python string file

---
+++ python/string.py
@@ -0,0 +1,11 @@
+loud_string = "This string is going to get sliced down!!!!"
+quiet_string = loud_string[:-4]
+
+print(quiet_string)
+
+string_one = "Uno"
+string_one += " means one in spanish."
+
+print(string_one)
+
+
