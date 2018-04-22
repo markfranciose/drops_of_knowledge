eed8d940347b1cb77afbcfd7043da6b3eeb382a4
Mark Franciose <mark.franciose@gmail.com>
2018-04-22 17:38:07 -0500

    more work on a closure example

--- js/closure.js
+++ js/closure.js
@@ -47,8 +47,11 @@ var size12 = makeSizer(12);
 var size14 = makeSizer(14);
 var size16 = makeSizer(16);
 
-document.getElementById('size-12').onclick = size12;
-document.getElementById('size-14').onclick = size14;
-document.getElementById('size-16').onclick = size16;
-
+try {
+	document.getElementById('size-12').onclick = size12;
+	document.getElementById('size-14').onclick = size14;
+	document.getElementById('size-16').onclick = size16;
+} catch(error) {
+	console.log("We aren't in the DOM anymore....");
+}
 
