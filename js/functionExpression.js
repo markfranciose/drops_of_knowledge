2414e5b3762e47f48719e141f367355f2597ee97
Mark Franciose <mark.franciose@gmail.com>
2018-06-13 21:23:27 -0500

    add js function expression

--- js/functionExpression.js
+++ js/functionExpression.js
@@ -2,9 +2,16 @@
 // like assigning a variable to a function object
 
 // function expressions are not hoisted
-// line below will error on not defined variable
-//e upperCaseExpression("small letters");
 
+try {
+	upperCaseExpression("mark p. franciose");
+}
+catch (error){
+	console.error(error);
+}
+finally {
+console.log("Function expressions are not hoisted");
+}
 
 let upperCaseExpression = function(word) {
     return word.toUpperCase();
